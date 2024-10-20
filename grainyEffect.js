8a0da558468a7f9a680ae9eb692e5d0668cf520e
André <60563950+andrexxrojas@users.noreply.github.com>
2024-10-20 19:45:20 +0800

    Add files via upload

---
+++ grainyEffect.js
@@ -0,0 +1,11 @@
+var options = {
+    animate: false,
+    patternWidth: 50,
+    patternHeight: 100,
+    grainOpacity: 0.05,
+    grainDensity: 1,
+    grainWidth: 1,
+    grainHeight: 1
+}
+
+grained("#grainy-bg", options)
---
+++ oxygenMonitor.js
@@ -0,0 +1,96 @@
+let nav = document.querySelector("nav")
+let content = document.querySelector(".container")
+
+nav.addEventListener("click", (e) => {
+    if(e.target == nav) {
+        if(nav.classList.contains("inactive")) {
+            nav.classList.remove("inactive")
+            content.style.marginLeft = "236px"
+        } else {
+            nav.classList.add("inactive")
+            content.style.marginLeft = "104px";
+        }
+    }
+})
+
+// When clicking on notification menu
+let notifArr = document.querySelectorAll(".notification-menu .notif p");
+
+notifArr.forEach((notification) => {
+    notification.addEventListener("click", () => {
+        const currentWhiteSpace = window.getComputedStyle(notification).whiteSpace;
+        
+        if(currentWhiteSpace === "nowrap") {
+            notification.style.whiteSpace = "normal";
+        } else {
+            notification.style.whiteSpace = "nowrap";
+        }
+    })
+});
+
+// When clicking notification icon
+let notifBtn = document.querySelector("nav ul .notif")
+let notifMenu = document.querySelector(".notification-menu")
+
+notifBtn.addEventListener("click", () => {
+    let notifDisplay = window.getComputedStyle(notifMenu).display
+
+    if(notifDisplay == "none") {
+        notifMenu.style.display = "flex"
+    }
+})
+
+document.addEventListener("click", (e) => {
+    if (!notifMenu.contains(e.target) && !notifBtn.contains(e.target)) {
+        notifMenu.style.display = "none"
+    }
+})
+
+// Prevent menu from closing when clicking inside it
+notifMenu.addEventListener("click", (e) => {
+    e.stopPropagation();
+})
+
+// Populating chart
+document.addEventListener("DOMContentLoaded", function () {
+    let myChart = document.getElementById("myChart").getContext("2d");
+
+    new Chart(myChart, {
+        type: "line",
+        data:{
+            labels: Array.from({length: 50}, (_, i) => i),
+            datasets: [{
+                label: "Oxygen Saturation Monitor",
+                data: [1, 1, 40, 50,4,5,6,7,78,8,7,5,5,34,23,23,42,34,234],
+                borderColor: 'red',
+                borderWidth: 2,
+                fill: false,
+                tension: 0.4
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Time (seconds)'
+                    }
+                },
+                y: {
+                    display: true,
+                    suggestedMin: -30,
+                    suggestedMax: 30,
+                    title: {
+                        display: true,
+                        text: 'Percentage'
+                    }
+                }
+            },
+            animation: {
+                duration: 2000,
+                easing: 'linear'
+            }
+        }
+    });
+});
